5a8c9f1829fa88f9ba9dd2477f9d18e2d881da20
Ayanaw Mengesha <ayanawmengesha21@gmail.com>
2024-12-29 13:45:02 -0800

    completed Task

--- src/App.jsx
+++ src/App.jsx
@@ -9,8 +9,8 @@ import Task3 from './component/Task3'
 function App() {
   return (
     <div>
-      {/* <Task1/> */}
-      {/* <Task2/> */}
+      <Task1/>
+      <Task2/>
       <Task3 name="Ayanaw" age="22" email="ayanawmengesha21@gmail.com" background="lightblue"/>
     </div>
   )
